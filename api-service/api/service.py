1dea7705b9f96de8c3dd075bf71dad408d7ad742
10686142 <vazz_m@hotmail.com>
2021-11-10 21:08:31 +0100

    Added api-service and frontend simple to git (removed from gitignore)

---
+++ api-service/api/service.py
@@ -0,0 +1,69 @@
+import os
+from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+import asyncio
+from api.tracker import TrackerService
+import pandas as pd
+from api.local import RUN_LOCAL
+
+# Initialize Tracker Service
+tracker_service = TrackerService()
+
+# Setup FastAPI app
+app = FastAPI(
+    title="API Server",
+    description="API Server",
+    version="v1"
+)
+
+# Enable CORSMiddleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_credentials=False,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Call the tracker service when the API service starts running.
+@app.on_event("startup")
+async def startup():
+    # Startup tasks
+    # Start the tracker service
+    asyncio.create_task(tracker_service.track())
+
+
+@app.get("/leaderboard")
+def leaderboard_fetch():
+    leaderboard_path ="/persistent/experiments/leaderboard.csv"
+
+    if RUN_LOCAL:
+        # Pyenv root is inside this folder
+        path_lead =  "../../persistent-folder/experiments/leaderboard.csv"
+        leaderboard_path = os.path.join(os.path.dirname(__file__),path_lead)
+
+      
+    # Fetch leaderboard
+    df = pd.read_csv(leaderboard_path)
+
+    df["id"] = df.index
+    df = df.fillna('')
+
+    return df.to_dict('records')
+
+
+
+# Routes
+@app.get("/")
+async def get_index():
+    return {
+        "message": "Welcome to the API Service"
+    }
+
+
+# Routes
+@app.get("/mushr")
+async def get_index():
+    return {
+        "message": "Mushrooms test"
+    }
