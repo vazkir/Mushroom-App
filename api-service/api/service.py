a6b795dc4379495c082f38f2b3caac7edbc88a31
10686142 <vazz_m@hotmail.com>
2021-11-10 21:12:00 +0100

    Added model.py and paths for it, along with fix to run it locally

--- api-service/api/service.py
+++ api-service/api/service.py
@@ -1,10 +1,15 @@
 import os
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
+from fastapi import File
+from tempfile import TemporaryDirectory
+from api import model
 import asyncio
 from api.tracker import TrackerService
 import pandas as pd
 from api.local import RUN_LOCAL
+
+
 
 # Initialize Tracker Service
 tracker_service = TrackerService()
@@ -31,6 +36,35 @@ async def startup():
     # Startup tasks
     # Start the tracker service
     asyncio.create_task(tracker_service.track())
+
+@app.get("/best_model")
+async def get_best_model():
+    model.check_model_change()
+    if model.best_model is None:
+        return {"message": 'No model available to serve'}
+    else:
+        return {
+            "message": 'Current model being served:'+model.best_model["model_name"],
+            "model_details": model.best_model
+        }
+
+
+@app.post("/predict")
+async def predict(
+        file: bytes = File(...)
+):
+    print("predict file:", len(file), type(file))
+
+    # Save the image
+    with TemporaryDirectory() as image_dir:
+        image_path = os.path.join(image_dir, "test.png")
+        with open(image_path, "wb") as output:
+            output.write(file)
+
+        # Make prediction
+        prediction_results = model.make_prediction(image_path)
+
+    return prediction_results
 
 
 @app.get("/leaderboard")
